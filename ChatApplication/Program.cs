af0f8b68ed913d18dec6da37516169416938e17f
Simon <simon.bruguerolle@hotmail.fr>
2018-03-26 15:13:33 +0200

    Add basic listenning socket

--- ChatApplication/Program.cs
+++ ChatApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatApplication
 {
@@ -6,7 +8,28 @@ namespace ChatApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Socket server = new Socket(AddressFamily.InterNetwork, 
+                SocketType.Stream, 
+                ProtocolType.Tcp);
+            
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4200);
+
+            server.Bind(localEndPoint);
+            // SUPPORT 100 USERS BEFORE SENDING
+            server.Listen(100);
+
+            Socket childSocket = server.Accept();
+            Console.WriteLine("Waiting for a connection...");
+            server.BeginAccept(new AsyncCallback(ar =>
+            {
+                Console.Write(ar.ToString()); 
+            }), server);
+            
+//            server.BeginReceive(new AsyncCallback(ar =>
+//            {
+//                Console.Write(ar.ToString());
+//            }), server)
+
         }
     }
 }
