1e0b98e27cd327d2cc1bafca50f7f3a29f43f4df
Simon <simon.bruguerolle@hotmail.fr>
2018-03-26 14:31:30 +0200

    Add message model

---
+++ ChatApplication/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChatApplication
+{
+    public class Message
+    {
+        private String     clientName;
+        private String     message;
+        private DateTime   sentAt;
+
+        public string ClientName
+        {
+            get => clientName;
+            set => clientName = value;
+        }
+
+        public string Message1
+        {
+            get => message;
+            set => message = value;
+        }
+
+        public DateTime SentAt
+        {
+            get => sentAt;
+            set => sentAt = value;
+        }
+    }
+}
