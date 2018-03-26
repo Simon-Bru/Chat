20ba61f48406758643e405e3ee0c4455925d62b4
Jean-Baptiste Crespo <jbc.waagh@gmail.com>
2018-03-26 16:16:15 +0200

    solution stream

--- ChatApplication/Program.cs
+++ ChatApplication/Program.cs
@@ -8,23 +8,55 @@ namespace ChatApplication
     {
         static void Main(string[] args)
         {
-            Socket server = new Socket(AddressFamily.InterNetwork, 
-                SocketType.Stream, 
-                ProtocolType.Tcp);
-            
-            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4200);
-
-            server.Bind(localEndPoint);
-            // SUPPORT 100 USERS BEFORE SENDING
-            server.Listen(100);
-
-            Socket childSocket = server.Accept();
-            Console.WriteLine("Waiting for a connection...");
-            server.BeginAccept(new AsyncCallback(ar =>
+            string message;
+            do
             {
-                Console.Write(ar.ToString()); 
-            }), server);
+               message=Run();
+               Console.WriteLine(message);
+            } while (message != "/shutdown");
+        }
+        
+        static string Run()
+        {
+            try{
+            TcpListener server = new TcpListener(IPAddress.Any, 4200);
+            server.Start();
+                String responseData = String.Empty;
             
+                do
+                { Byte[] data = new Byte[256];
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                   
+                    
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received : " + responseData);
+                    data = new Byte[256];
+                    data = System.Text.Encoding.ASCII.GetBytes("hello from server");
+                    stream.Write(data, 0, data.Length);
+                    stream.Close();
+                    client.Close();
+                } while (responseData != "/shutdown");
+           
+                //server.EndAcceptTcpClient();
+                
+               
+                
+                
+                return responseData;
+                
+            }
+                catch (ArgumentNullException e) 
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            } 
+            catch (SocketException e) 
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            return "running...";
 //            server.BeginReceive(new AsyncCallback(ar =>
 //            {
 //                Console.Write(ar.ToString());
